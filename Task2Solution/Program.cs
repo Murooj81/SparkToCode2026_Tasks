0dcd3fa2bcca858d4390be80b180aed1281ce61b
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-05 02:06:20 +0400

    Implemented Peoblem 1 (Countdown Timer)

--- Task2Solution/Program.cs
+++ Task2Solution/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Task2Solution
+﻿using System.Runtime.InteropServices;
+
+namespace Task2Solution
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Countdown Timer
+            // Some Code To Solove The Task
+
+            Console.WriteLine("Enter the countdown time: ");
+            int countdownTime = int.Parse(Console.ReadLine());
+             
+            for (int i = countdownTime; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(1000);
+
+
+            }
+            Console.WriteLine("Countdown finished!");
+
+            ///////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
+         
 }
