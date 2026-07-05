9a0e54b1528eaa7c91f9942c2b1fadb6b90108e1
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-05 19:20:59 +0400

    Solve Problem 1 ( Absolute Difference)

--- Task3slove/Program.cs
+++ Task3slove/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"
+            //1. Absolute Difference
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double difference = num1 - num2;
+            double absoluteDifference = Math.Abs(difference);
+
+            Console.WriteLine($"The absolute difference is: {absoluteDifference}");
+
+            //////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
