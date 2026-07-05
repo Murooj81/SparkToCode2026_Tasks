45674d8e8b4d8740db5e7d600f00b6e1efc255ff
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-06 02:04:14 +0400

    solve Task 1 - Personal Info Card

--- Task1Slution/Program.cs
+++ Task1Slution/Program.cs
@@ -4,7 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Task 1 - Personal Info Card
+
+            String name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Hight: " + height);
+            Console.WriteLine("Student: " + isStudent);
+            
+            ////////////////////////////////////////////////
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
