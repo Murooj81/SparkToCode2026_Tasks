abbc7dbc5e7255fbbf5c98ef22f6fe21723779f4
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-05 20:32:33 +0400

    solve problem 10-Word Position Finder

--- Task3slove/Program.cs
+++ Task3slove/Program.cs
@@ -163,7 +163,29 @@
             Console.WriteLine("Round Down: " + Math.Floor(number));
 
             ////////////////////////////////////////////////////////////////
-           
+
+            //10-Word Position Finder
+
+
+            Console.Write("Enter sentence: ");
+            string sentence = Console.ReadLine();
+
+            Console.Write("Enter word: ");
+            string word = Console.ReadLine();
+
+            int first = sentence.IndexOf(word);
+            int last = sentence.LastIndexOf(word);
+
+            if (first == -1)
+            {
+                Console.WriteLine("Word not found");
+            }
+            else
+            {
+                Console.WriteLine("First Position: " + first);
+                Console.WriteLine("Last Position: " + last);
+            }
+
 
 
 
