861bbab5c3419c528565ce501d4467762986c675
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-06 02:22:45 +0400

    solve Task 9 - Day Name Finder

--- Task1Slution/Program.cs
+++ Task1Slution/Program.cs
@@ -174,9 +174,25 @@ namespace Task1Slution
 
             //////////////////////////////////////////////////////////////
 
+            //Task 9 - Day Name Finder
 
 
+            Console.Write("Enter a number (1-7): ");
+            int dayNum = int.Parse(Console.ReadLine());
 
+            switch (dayNum)
+            {
+                case 1: Console.WriteLine("Sunday"); break;
+                case 2: Console.WriteLine("Monday"); break;
+                case 3: Console.WriteLine("Tuesday"); break;
+                case 4: Console.WriteLine("Wednesday"); break;
+                case 5: Console.WriteLine("Thursday"); break;
+                case 6: Console.WriteLine("Friday"); break;
+                case 7: Console.WriteLine("Saturday"); break;
+                default: Console.WriteLine("Invalid day number"); break;
+            }
+            /////////////////////////////////////////////////////////////
+            
 
 
 
