cf2be1043b79f5990bec6333a6efba16c6705ca0
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-06 01:46:54 +0400

    add project file

---
+++ Task1Slution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1Slution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
