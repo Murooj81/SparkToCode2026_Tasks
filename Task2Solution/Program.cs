fb31d811929059ef1e5f4f32d56bdd27237c540b
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-05 04:09:35 +0400

    implemnted Problem 9 ( Validated Positive Number Input)

--- Task2Solution/Program.cs
+++ Task2Solution/Program.cs
@@ -179,25 +179,51 @@ namespace Task2Solution
             }
 
             Console.WriteLine("The final sum of even numbers is: " + evenSum);
-             
+
             /////////////////////////////////////////////////////////////////////////
+
+            //8- Validated Positive Number Input
+            // Some Code To Solove The Task 
+
+            int validPositiveNum = 0;
+            bool isValidInput = false;
+
             
+            do
+            {
+                try
+                {
+                    Console.Write("Please enter a positive number: ");
+                    validPositiveNum = int.Parse(Console.ReadLine());
 
+                    
+                    if (validPositiveNum <= 0)
+                    {
+                        Console.WriteLine("Error: Number must be greater than zero.");
+                        isValidInput = false;
+                    }
+                    else
+                    {
+                        isValidInput = true;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Input is not a valid whole number.");
+                    isValidInput = false;
+                }
+            } while (!isValidInput);
 
+            
+            int validatedLoopSum = 0;
+            for (int i = 1; i <= validPositiveNum; i++)
+            {
+                validatedLoopSum += i;
+            }
+            Console.WriteLine("The sum of numbers from 1 to " + validPositiveNum + " is: " + validatedLoopSum);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            ///////////////////////////////////////////////
+            
 
 
 
