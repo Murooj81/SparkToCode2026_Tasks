4adc11355f80f2640389566cd7d897bc448b8555
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-06 17:47:37 +0400

    solved Task 6 - Rectangle Area & Perimeter Functions

--- Task4Slution/Program.cs
+++ Task4Slution/Program.cs
@@ -81,7 +81,31 @@
                 return number % 2 == 0;
             }
 
+            ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+            // Task 6 - Rectangle Area & Perimeter Functions
+
+            Console.Write("Enter rectangle length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter rectangle width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = CalculateArea(length, width);
+            double perimeter = CalculatePerimeter(length, width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
+
+            static double CalculateArea(double length, double width)
+            {
+                return length * width;
+            }
+            static double CalculatePerimeter(double length, double width)
+            {
+                return 2 * (length + width);
+            }
+            /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
