8f2ea3789dbbf72ca85ad35aaecaf87e687e0a2e
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-06 19:51:41 +0400

    i tryed to slove Task 12 - Student Report Card Generator

--- Task4Slution/Program.cs
+++ Task4Slution/Program.cs
@@ -4,6 +4,11 @@ namespace Task4Slution
 {
     internal class Program
     {
+        private static object name;
+        private static object avg;
+        private static object grade;
+        private static object printReportCard;
+
         static void Main(string[] args)
         {
 
@@ -12,11 +17,11 @@ namespace Task4Slution
             Console.Write("Enter your name: ");
             string nameInput = Console.ReadLine();
             PrintWelcome(nameInput);
-        }
 
-        static void PrintWelcome(string name)
-        {
-            Console.WriteLine("Welcome back, " + name + "! Have a fantastic learning session.");
+            static void PrintWelcome(string name)
+            {
+                Console.WriteLine("Welcome, " + name + "!");
+            }
 
             /////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -191,105 +196,45 @@ namespace Task4Slution
 
             //////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            //Task 12 - Student Report Card Generator
+
+            Console.Write("Enter student name: ");
+            string studentName = Console.ReadLine();
+
+            Console.Write("Enter student score 1: ");
+            double score1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter student score 2: ");
+            double score2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter student score 3: ");
+            double score3 = double.Parse(Console.ReadLine());
+
+            double avg = (score1 + score2 + score3);
+            string grade = GetGradeLetter((int)avg);
+
+            printReportCard((string)name, avg, grade);
+
+            static double calculateAverage(double s1, double s2, double s3)
+            {
+                return (s1 + s2 + s3) / 3;
+            }
+            //static string GetGradeLetter(double average)
+            //{
+                //if (average >= 90) return "A";
+                //else if (average >= 80) return "B";
+                //else if (average >= 70) return "C";
+                //else if (average >= 60) return "D";
+                //else return "F";
+            //}
+
+            static void printReportCard(string name, double avg, string grade)
+            {
+                Console.WriteLine("Student Name: " + name);
+                Console.WriteLine("Average Score: " + avg);
+                Console.WriteLine("Grade: " + grade);
+
+            }
 
         }
     }
