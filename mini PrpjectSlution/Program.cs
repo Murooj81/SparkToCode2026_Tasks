356994bdae391bb7b68e247983f56d0a25165fb1
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-11 22:40:36 +0400

    Add new project

---
+++ mini PrpjectSlution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mini_PrpjectSlution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
