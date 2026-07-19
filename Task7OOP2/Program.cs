cd3fc4c90c2f97d0c19981dcc0c690cc5fa69760
Murooj Al Shehaibi <murooj459@gmail.com>
2026-07-19 19:00:25 +0400

    add class Guest

--- Task7OOP2/Program.cs
+++ Task7OOP2/Program.cs
@@ -23,7 +23,33 @@
         }
     }
 
+    internal class Guest
+    {
+        public string GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+        public string CheckInDate { get; set; }
+        public int TotalNights { get; set; }
 
+        public Guest(string guestId, string guestName, string checkInDate, int totalNights)
+        {
+            GuestId = guestId;
+            GuestName = guestName;
+            RoomNumber = "Not Assigned";
+            CheckInDate = checkInDate;
+            TotalNights = totalNights;
+        }
+
+        public void DisplayGuest()
+        {
+            Console.WriteLine($"ID: {GuestId,-5} | Name: {GuestName,-12} | Room: {RoomNumber,-12} | Check-in: {CheckInDate,-10} | Nights: {TotalNights}");
+        }
+
+        public double CalculateTotalCost(double pricePerNight)
+        {
+            return TotalNights * pricePerNight;
+        }
+    }
 
 
 
